0054ab279503950b7cbaa038584166bbca657df8
TrailSix <info@trailsix.com>
2021-06-14 03:32:36 +0000

    Setting up our React app

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Intnt</h1>
+        <p className="text-muted">A simple intent based app</p>
+      </div>
+    </div>
+  );
+}
